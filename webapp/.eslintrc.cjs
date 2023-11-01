e9170368a135b80c0e881611b8855e678750f299
Romain Chaffal <romain.chaffal@passculture.app>
2023-11-01 19:33:32 +0100

    chore: configure eslint, still broken in api/ when launched recursively...

--- webapp/.eslintrc.cjs
+++ webapp/.eslintrc.cjs
@@ -1,15 +1,23 @@
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "../.eslintrc.json",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "react"],
+  settings: {
+    react: {
+      version: "detect",
+    }
+  },
   rules: {
+    'react/jsx-filename-extension': [1, { extensions: [".ts", ".tsx"] }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
