082f913412e45862b593d12f070116ffae51bc71
Romain Chaffal <romain.chaffal@passculture.app>
2023-11-01 21:06:43 +0100

    chore: configure prettier

--- webapp/.eslintrc.cjs
+++ webapp/.eslintrc.cjs
@@ -1,26 +1,25 @@
-
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "../.eslintrc.json",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react/recommended",
+    '../.eslintrc.json',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', "react"],
+  plugins: ['react-refresh', 'react'],
   settings: {
     react: {
-      version: "detect",
-    }
+      version: 'detect',
+    },
   },
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
-    'react/jsx-filename-extension': [1, { extensions: [".ts", ".tsx"] }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
